80f5ce26a29ad8e0aca8b145ec3ac4f0e96babea
Simpson, William O <william-simpson@uiowa.edu>
2020-10-26 01:48:30 -0500

    Adding more methods for checking/updating Users

--- src/main/java/me/wildev/planner/data/User.java
+++ src/main/java/me/wildev/planner/data/User.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.springframework.util.Assert.notNull;
+
 @Entity
 @Table(name="users",
         uniqueConstraints = {
@@ -40,4 +42,42 @@ public class User
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String password, String email, Role role)
+    {
+        notNull(username, "Method called with null parameter (username)");
+        notNull(password, "Method called with null parameter (password)");
+        notNull(email, "Method called with null parameter (email)");
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles.add(role);
+    }
+
+    public boolean hasRole(Role givenRole)
+    {
+        for(Role currentRole : roles)
+            if(currentRole.equals(givenRole))
+                return true;
+
+        return false;
+    }
+
+    public boolean addRole(Role role)
+    {
+        return roles.add(role);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof User)
+        {
+            User user2 = (User) obj;
+
+            return id.equals(user2.getId());
+        }
+
+        return false;
+    }
 }
