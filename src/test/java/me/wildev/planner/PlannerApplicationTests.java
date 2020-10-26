23f076097f99b9c6e01861b5232e8aff7ad97d64
Simpson, William O <william-simpson@uiowa.edu>
2020-10-26 01:09:04 -0500

    Initial commit

---
+++ src/test/java/me/wildev/planner/PlannerApplicationTests.java
@@ -0,0 +1,16 @@
+package me.wildev.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
