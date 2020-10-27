e256d75f9fe95e03f6349310abf96c478fa4e43a
Simpson, William O <william-simpson@uiowa.edu>
2020-10-27 06:13:35 -0500

    Added header and footers

--- src/webapp/src/app/app.module.ts
+++ src/webapp/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
